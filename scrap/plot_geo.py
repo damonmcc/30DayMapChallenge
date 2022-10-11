be086f7f52af89bd5f2c9207a16575c92a06c63c
Damon McCullough <dmccullough@policingequity.org>
2022-10-11 10:34:20 -0400

    city council districts

--- scrap/plot_geo.py
+++ scrap/plot_geo.py
@@ -2,15 +2,15 @@
 
 from src.data_io import (
     construct_file_suffix_now,
-    load_nyc_borough_boundaries,
+    load_nyc_city_council_districts,
     save_axis_to_png,
 )
 from src.vizualize import map_boundaries
 
 
 if __name__ == "__main__":
-    geo = load_nyc_borough_boundaries()
-    geo_name = "NYC Boroughs"
+    geo = load_nyc_city_council_districts()
+    geo_name = "NYC City Council Districts"
 
     geo_ax = map_boundaries(geo, geo_name)
     save_axis_to_png(geo_ax, f"{geo_name}_{construct_file_suffix_now()}")
