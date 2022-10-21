756895ebeaa1dd42adda3edd2efa0848a374ae17
Damon McCullough <dmccullough@policingequity.org>
2022-10-20 22:36:09 -0400

    coordinates and outliers

--- src/transform.py
+++ src/transform.py
@@ -1,4 +1,5 @@
 import geopandas as gpd
+import numpy as np
 import pandas as pd
 
 
@@ -6,8 +7,47 @@ def combine_geodataframes(geodataframes: list[gpd.GeoDataFrame]) -> gpd.GeoDataF
     return pd.concat(geodataframes)
 
 
-def map_borough_code_to_name(
-    data: pd.DataFrame, borough_code_column: str
+def zeroes_to_nan(
+    data: pd.DataFrame,
+    field: str,
+):
+    data[field] = data[field].replace(0, np.nan)
+    return data
+
+
+def outliers_to_ceiling(
+    data: pd.DataFrame,
+    field: str,
+    ceiling: float = None,
+) -> pd.DataFrame:
+    default_upper_quantile = 0.99
+    if not ceiling:
+        ceiling = data[field].quantile(default_upper_quantile)
+    data[field].loc[data[field] > ceiling] = ceiling
+
+    return data
+
+
+def outliers_to_nan(
+    data: pd.DataFrame,
+    field: str,
+) -> pd.DataFrame:
+    upper_limit = data[field].quantile()
+
+    field_transformed = data[field].dropna()
+    field_transformed = field_transformed.sort_values()
+    if outliers_to_drop[0]:
+        field_transformed = field_transformed.iloc[outliers_to_drop[0] :]
+    if outliers_to_drop[1]:
+        field_transformed = field_transformed.iloc[: -outliers_to_drop[1]]
+    data[field] = field_transformed
+
+    return data
+
+
+def decode_borough_code_to_name(
+    data: pd.DataFrame,
+    borough_code_column: str,
 ) -> pd.DataFrame:
     mapping_dict = {
         "BX": "Bronx",
@@ -44,6 +84,12 @@ def aggregate_counts_by_fields(
 #     return
 
 
+def construct_coordinates(lat, lon):
+    if pd.isnull(lat) or pd.isnull(lon):
+        return np.nan
+    return ", ".join([str(lat), str(lon)])
+
+
 def merge_boundaries_and_data(
     data: pd.DataFrame,
     data_area_column: str,
@@ -55,8 +101,12 @@ def merge_boundaries_and_data(
     data_counts = aggregate_counts_by_fields(data, [data_area_column])
 
     # align values to merge with
-    data_counts[data_area_column] = data_counts[data_area_column].str.title()
-    boundaries[boundaries_area_column] = boundaries[boundaries_area_column].str.title()
+    data_counts[data_area_column] = (
+        data_counts[data_area_column].astype(str).str.title()
+    )
+    boundaries[boundaries_area_column] = (
+        boundaries[boundaries_area_column].astype(str).str.title()
+    )
 
     # merge data and boundaries
     data_and_boundaries = gpd.GeoDataFrame(
